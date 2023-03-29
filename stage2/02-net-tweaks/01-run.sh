2fe3fac1159ca914302f1bbea5a7173e1c585314
nitish-liveeye <67207811+nitish-liveeye@users.noreply.github.com>
2023-03-29 16:14:49 -0700

    Update 01-run.sh

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -38,3 +38,7 @@ else
     echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-3f300000.mmcnr:wlan"
     echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-fe300000.mmcnr:wlan"
 fi
+
+on_chroot << EOF
+echo 'nymea:xxxx' | chpasswd
+EOF
